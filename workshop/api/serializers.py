e64061bafa697d8dc4765f72c3e57f404499dba1
aajain1301 <aajain1301@gmail.com>
2019-07-20 14:05:29 +0530

    model changes

--- workshop/api/serializers.py
+++ workshop/api/serializers.py
@@ -7,15 +7,19 @@ from registration.models import User
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User(
+            name=validated_data['name'],
+            contact=validated_data['contact'],
+            txn_id=validated_data['txn_id'],
             email=validated_data['email'],
             username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            contact=validated_data['contact'],
-            college_name=validated_data['college_name'],
-            branch_name=validated_data['branch_name'],
+            # first_name=validated_data['first_name'],
+            # last_name=validated_data['last_name'],
+            college=validated_data['college'],
+            branch=validated_data['branch'],
+            course=validated_data['course'],
             sem=validated_data['sem'],
-            address=validated_data['address']
+            address=validated_data['address'],
+            pay_mode=validated_data['pay_mode'],
         )
         user.set_password(validated_data['password'])
         user.save()
@@ -23,6 +27,5 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         extra_kwargs = {'password': {'write_only': True}}
-        fields = ('username','password','first_name','last_name','email','contact',
-            'college_name','branch_name','sem','address')
-
+        fields = ('username','password','name','email','contact','txn_id',
+            'college','branch','course','sem','address','pay_mode')
--- workshop/registration/models.py
+++ workshop/registration/models.py
@@ -4,14 +4,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    contact = models.PositiveIntegerField(default=1)
-    college_name = models.CharField(max_length=50)
-    branch_name = models.CharField(max_length=50)
-    sem = models.IntegerField(default=1)
+    name = models.CharField(max_length=50)
+    # first_name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=50)
+    contact = models.PositiveIntegerField(default=1,unique=True)
+    txn_id  = models.CharField(max_length=40)
+    email   = models.EmailField(unique=True)
+    college = models.CharField(max_length=50)
+    branch  = models.CharField(max_length=50)
+    course  = models.CharField(max_length=50)
+    sem     = models.IntegerField(default=1)
     address = models.TextField(blank=True)
+    pay_mode= models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.first_name
